98204c8d98a1b80d5da493cd57a98a32b24e1241
Ayush Singhal <ayushsinghals02@gmail.com>
2023-08-12 19:53:20 +0530

    Initial Commit

---
+++ Coding Kontest/Coding_KontestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coding_KontestApp.swift
+//  Coding Kontest
+//
+//  Created by Ayush Singhal on 12/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Coding_KontestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coding Kontest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coding Kontest
+//
+//  Created by Ayush Singhal on 12/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
