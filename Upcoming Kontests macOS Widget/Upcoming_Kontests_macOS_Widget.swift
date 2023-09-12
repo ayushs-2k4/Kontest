799e97c735a370b4782279591a6858b523bd7680
Ayush Singhal <ayushsinghals02@gmail.com>
2023-09-12 12:30:24 +0530

    Added Screens for iOS and macOS Widgets.

--- Upcoming Kontests macOS Widget/Upcoming_Kontests_macOS_Widget.swift
+++ Upcoming Kontests macOS Widget/Upcoming_Kontests_macOS_Widget.swift
@@ -5,52 +5,115 @@
 //  Created by Ayush Singhal on 12/09/23.
 //
 
-import WidgetKit
 import SwiftUI
+import WidgetKit
 
 struct Provider: TimelineProvider {
+    let kontestModel = KontestModel.from(dto: KontestDTO(name: "ProjectEuler+1", url: "https://hackerrank.com/contests/projecteuler", start_time: "2023-08-15 18:29:00 UTC", end_time: "2023-08-18 17:43:00 UTC", duration: "1020.0", site: "HackerRank", in_24_hours: "No", status: "BEFORE"))
+
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
+        SimpleEntry(date: Date(), error: nil, ongoingKontests: [kontestModel], laterTodayKontests: [], tomorrowKontests: [], laterKontests: [])
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        let entry = SimpleEntry(date: Date(), error: nil, ongoingKontests: [kontestModel], laterTodayKontests: [], tomorrowKontests: [], laterKontests: [])
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        let today = Date()
+        let tomorrow = CalendarUtility.getTomorrow()
+        let dayAfterTomorrow = CalendarUtility.getDayAfterTomorrow()
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
+        Task {
+            let allKontestsWithError = await GetKontests.getKontests()
+            let allKontests = allKontestsWithError.fetchedKontests
+            let error = allKontestsWithError.error
+
+            let ongoingKontests = allKontests.filter { CalendarUtility.isKontestRunning(kontestStartDate: CalendarUtility.getDate(date: $0.start_time) ?? today, kontestEndDate: CalendarUtility.getDate(date: $0.end_time) ?? today) }
+
+            let laterTodayKontests = allKontests.filter {
+                CalendarUtility.getDate(date: $0.start_time) ?? today < tomorrow && !(ongoingKontests.contains($0))
+            }
+
+            let tomorrowKontests = allKontests.filter { (CalendarUtility.getDate(date: $0.start_time) ?? today >= tomorrow) && (CalendarUtility.getDate(date: $0.start_time) ?? today < dayAfterTomorrow) }
+
+            let laterKontests = allKontests.filter {
+                CalendarUtility.getDate(date: $0.start_time) ?? today >= dayAfterTomorrow
+            }
+
+            var myEntries: [SimpleEntry] = []
+
+            let entry = SimpleEntry(date: Date(), error: error, ongoingKontests: ongoingKontests, laterTodayKontests: laterTodayKontests, tomorrowKontests: tomorrowKontests, laterKontests: laterKontests)
+
+            myEntries.append(entry)
+
+            let timeline = Timeline(entries: myEntries, policy: .atEnd)
+            completion(timeline)
         }
+    }
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+    private func fetchKontests() async -> ([KontestModel], String?) {
+        let repository = KontestRepository()
+
+        do {
+            let fetchedKontests = try await repository.getAllKontests()
+
+            return (fetchedKontests
+                .map { dto in
+                    let kontest = KontestModel.from(dto: dto)
+                    // Load Reminder status
+                    kontest.loadReminderStatus()
+                    return kontest
+                }
+                .filter { kontest in
+                    let kontestDuration = CalendarUtility.getFormattedDuration(fromSeconds: kontest.duration) ?? ""
+                    let kontestEndDate = CalendarUtility.getDate(date: kontest.end_time)
+                    let isKontestEnded = CalendarUtility.isKontestOfPast(kontestEndDate: kontestEndDate ?? Date())
+
+                    return !kontestDuration.isEmpty && !isKontestEnded
+                }, nil)
+        } catch {
+            print("error in fetching all Kontests: \(error)")
+            let kontestModel = KontestModel.from(dto: KontestDTO(name: "ProjectEuler+1indsjjns,xd", url: "https://hackerrank.com/contests/projecteuler", start_time: "2023-08-15 18:29:00 UTC", end_time: "2023-08-18 17:43:00 UTC", duration: "1020.0", site: "HackerRank", in_24_hours: "No", status: "BEFORE"))
+            return ([kontestModel], error.localizedDescription)
+        }
     }
 }
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let emoji: String
+    let error: Error?
+    let ongoingKontests: [KontestModel]
+    let laterTodayKontests: [KontestModel]
+    let tomorrowKontests: [KontestModel]
+    let laterKontests: [KontestModel]
 }
 
-struct Upcoming_Kontests_macOS_WidgetEntryView : View {
+struct Upcoming_Kontests_macOS_WidgetEntryView: View {
     var entry: Provider.Entry
 
     var body: some View {
         VStack {
-            HStack {
-                Text("Time:")
-                Text(entry.date, style: .time)
-            }
+            if let error = entry.error {
+                Text("Error: \(error.localizedDescription)")
+            } else {
+                if !entry.ongoingKontests.isEmpty {
+                    CreateSectionView(title: "On Going Kontests", kontests: entry.ongoingKontests, toShowDate: false, toShowTime: false)
+                }
 
-            Text("Emoji:")
-            Text(entry.emoji)
+                if !entry.laterTodayKontests.isEmpty {
+                    CreateSectionView(title: "Later Today Kontests", kontests: entry.laterTodayKontests, toShowDate: false, toShowTime: true)
+                }
+
+                if !entry.tomorrowKontests.isEmpty {
+                    CreateSectionView(title: "Tomorrow Kontests", kontests: entry.tomorrowKontests, toShowDate: false, toShowTime: true)
+                }
+
+                if !entry.laterKontests.isEmpty {
+                    CreateSectionView(title: "Later Kontests", kontests: entry.laterKontests, toShowDate: true, toShowTime: false)
+                }
+            }
         }
     }
 }
@@ -69,7 +132,9 @@ struct Upcoming_Kontests_macOS_Widget: Widget {
                     .background()
             }
         }
+        .supportedFamilies([.systemLarge, .systemExtraLarge])
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
     }
 }
+ 
