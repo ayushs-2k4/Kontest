c231bf9b258559afa970e39cf8b2b33f313f5e1b
Ayush Singhal <ayushsinghals02@gmail.com>
2023-09-17 19:52:53 +0530

    Added MyTest (Mock).

---
+++ KontestTests/MyTest.swift
@@ -0,0 +1,14 @@
+//
+//  MyTest.swift
+//  KontestTests
+//
+//  Created by Ayush Singhal on 17/09/23.
+//
+
+import XCTest
+
+class MyTest: XCTestCase {
+    func test() {
+        XCTAssertTrue(2 * 2 == 4)
+    }
+}
