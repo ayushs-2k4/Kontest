33e8c8b570e0e4ff49e004c0b20e8c3be13c6173
Ayush Singhal <ayushsinghals02@gmail.com>
2023-09-18 17:34:18 +0530

    Added some more tests.

--- KontestTests/AllKontestsViewModelTests.swift
+++ KontestTests/AllKontestsViewModelTests.swift
@@ -14,7 +14,7 @@ class AllKontestsViewModelTests: XCTestCase {
     override func setUp() async throws {
         viewModel = AllKontestsViewModel(
             notificationsViewModel: MockNotificationsViewModel(),
-            filterWebsitesViewModel: MockFilterWebsitesViewModel(),
+            filterWebsitesViewModel: MockFilterWebsitesViewModel(allowedWebsites: ["LeetCode", "CodeForces", "HackerEarth"]),
             repository: KontestsTestFakeRepository()
         )
     }
@@ -25,9 +25,22 @@ class AllKontestsViewModelTests: XCTestCase {
 
     func testInitialization() {
         let expectation = XCTNSPredicateExpectation(predicate: NSPredicate(block: { [weak self] _, _ in
-            self?.viewModel.allKontests.count == 2 && self?.viewModel.toShowKontests.count == 1
+            self?.viewModel.allKontests.count == 4 && self?.viewModel.toShowKontests.count == 3 && self?.viewModel.backupKontests.count == 3
         }), object: nil)
         wait(for: [expectation], timeout: 2)
+    }
+
+    func testFilterKontestsUsingSearchText() {
+        let expectation1 = XCTNSPredicateExpectation(predicate: NSPredicate(block: { [weak self] _, _ in
+            self?.viewModel.searchText = "LeetCode"
+            return self?.viewModel.toShowKontests.count == 1
+        }), object: nil)
+
+        let expectation2 = XCTNSPredicateExpectation(predicate: NSPredicate(block: { [weak self] _, _ in
+            self?.viewModel.searchText = "HackerEarth"
+            return self?.viewModel.toShowKontests.count == 1
+        }), object: nil)
+        wait(for: [expectation1, expectation2], timeout: 2)
     }
 }
 
@@ -48,7 +61,11 @@ class KontestsTestFakeRepository: KontestFetcher {
 
             KontestDTO(name: "1v1 Games by CodeChef", url: "https://www.codechef.com/GAMES", start_time: "2022-10-10 06:30:00 UTC", end_time: "2032-10-10 06:30:00 UTC", duration: "315619200.0", site: "CodeChef", in_24_hours: "No", status: "CODING"), // Expired
 
-            KontestDTO(name: "Weekly Contest 358", url: "https://leetcode.com/contest/weekly-contest-358", start_time: startDateString, end_time: endDateString, duration: "5400", site: "LeetCode", in_24_hours: "Yes", status: "BEFORE")
+            KontestDTO(name: "Weekly Contest 358", url: "https://leetcode.com/contest/weekly-contest-358", start_time: startDateString, end_time: endDateString, duration: "5400", site: "LeetCode", in_24_hours: "Yes", status: "BEFORE"),
+
+            KontestDTO(name: "CodeForces Round (Div. 2)", url: "https://codeforces.com/enter?back=%2FcontestRegistration%2F1871", start_time: startDateString, end_time: endDateString, duration: "\(86400 * 3)", site: "CodeForces", in_24_hours: "Yes", status: "CODING"),
+
+            KontestDTO(name: "Hacker Earth Coding", url: "https://hackerearth.com/", start_time: startDateString, end_time: endDateString, duration: "\(86400 * 3)", site: "HackerEarth", in_24_hours: "Yes", status: "CODING")
         ]
 
         return allKontests
