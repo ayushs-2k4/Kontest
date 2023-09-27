b1f16e56b6bab8411bd1ba46a2b23fc610c42762
Ayush Singhal <ayushsinghals02@gmail.com>
2023-09-28 00:42:38 +0530

    Removed id from KontestDTO.

--- Kontest/DTOs/KontestDTO.swift
+++ Kontest/DTOs/KontestDTO.swift
@@ -8,22 +8,9 @@
 import CryptoKit
 import Foundation
 
-struct KontestDTO: Codable, Identifiable {
-    var id: String { generateUniqueID() }
-
+struct KontestDTO: Codable {
     let name: String
     let url: String
     let start_time, end_time, duration, site: String
     let in_24_hours, status: String
-
-    private func generateUniqueID() -> String {
-        let combinedString = "\(name)\(url)\(start_time)\(end_time)\(duration)\(site)"
-
-        if let data = combinedString.data(using: .utf8) {
-            let hash = SHA256.hash(data: data)
-            return hash.compactMap { String(format: "%02x", $0) }.joined()
-        } else {
-            return UUID().uuidString
-        }
-    }
 }
